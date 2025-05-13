91f1a398c7393505eef719ca3aa59997e9101150
Comeoksal <keumsiun0503@gmail.com>
2025-05-13 17:35:28 +0900

    1

--- sysp/work2/2.c
+++ sysp/work2/2.c
@@ -14,7 +14,7 @@ int main(){
 	pid_t pid;
 	
 	pid = fork();
-	if(pid>0){
+	if(pid==0){
 		printf("Input an expression : ");
 		if(fgets(buf, sizeof(buf), stdin) == NULL){
 			perror("input fail");
@@ -29,7 +29,9 @@ int main(){
 			perror("write fail");
 			exit(1);
 		}
-	} else if (pid == 0){
+	} else if (pid > 0){
+		while(wait(&status) !=pid)
+			continue;
 		read_and_print();
 	} else {
 		perror("fork fail");
@@ -39,13 +41,37 @@ int main(){
 }
 
 void read_and_print(){
-	int fd, nbyte, len, idx=0;
-	char c, buf[BUFSIZ];
+	int fd, nbyte, priorityValue=0, idx=0, isPriority=0;
+	char *token, buf[BUFSIZ], *stack[1024];
 
 	fd = open("input.txt", O_RDONLY);
 	if(fd==-1){
 		perror("open fail");
 		exit(1);
 	}
+	nbyte = read(fd, buf, sizeof(buf));
+	token = strtok(buf, "+-*/");
+	while(token!=NULL){
+		stack[idx] = malloc(10);
+		switch(token){
+			case '+':
+				stack[idx++]='+';
+				break;
+			case '-':
+				stack[idx++]='-';
+				break;
+			case '*':
+				idx--;
+				priorityValue=stack[idx];
+				isPriority=1;
+				break;
+			case '/':
+				idx--;
+				priorityValue=stack[idx];
+				isPriority=2;
+				break;
+			default:
 
+
+		token = strtok(NULL, "+-*/");
 }
